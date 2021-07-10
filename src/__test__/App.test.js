6dfcacdc295dc532acc1fcd8bc47ccc6653908b5
krdormer <krdormer_dev@protonmail.com>
2021-07-10 16:49:47 -0500

    Setup testing libraries
    Setup initial config and first test of rendering component. Will be ad
    ding further tests.

---
+++ src/__test__/App.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../App';
+
+describe('<App />', () => {
+    const wrapper = shallow(
+        <Router>
+            <App />
+        </Router>
+    );
+    it('renders <App /> component', () => {
+        expect(wrapper.find(App).length).toEqual(1);
+    });
+})
