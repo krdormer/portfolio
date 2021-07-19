d656310c6812dc98154f168fd9be9555436d662b
krdormer <krdormer_dev@protonmail.com>
2021-07-19 11:56:37 -0500

    Update TechPage styles with styled-components

--- src/pages/tech-page/tech-page.component.jsx
+++ src/pages/tech-page/tech-page.component.jsx
@@ -1,80 +1,149 @@
-// React and React hook imports 
-import React, { useState } from 'react';
-import './tech-page.styles.css';
+// React and React hook imports
+import React, { useState, useEffect } from 'react';
+// For styling
+import { TechPageStyles, TechPageTechContainer } from './tech-page.styles';
 import { Animated } from 'react-animated-css';
 
-// Layout each image one at a time? 
-
 const TechPage = () => {
-        // Sets a state hook for each <p> tag, used for timing of display on screen
-        const [visible_first, setVisibilityFirst] = useState(false)
-        const [visible_second, setVisibilitySecond] = useState(false);
-        const [visible_third, setVisibilityThird] = useState(false);
-        const [visible_fourth, setVisibilityFourth] = useState(false);
+  // Sets a state hook for each <p> tag, used for timing of display on screen
+  const [visible_first, setVisibilityFirst] = useState(false);
+  const [visible_second, setVisibilitySecond] = useState(false);
+  const [visible_third, setVisibilityThird] = useState(false);
+  const [visible_fourth, setVisibilityFourth] = useState(false);
 
+  useEffect(() => {
+    setInterval(() => {
+      setVisibilityFirst(true);
+    }, 1000);
 
+    setInterval(() => {
+      setVisibilitySecond(true);
+    }, 2000);
 
-        // Timers set to appropriate hook, displays one <p/> tag per second 
+    setInterval(() => {
+      setVisibilityThird(true);
+    }, 3000);
+    setInterval(() => {
+      setVisibilityFourth(true);
+    }, 4000);
+  }, []);
 
-        setInterval(() => {
-            setVisibilityFirst(true)
-        }, 1000)
-
-        setInterval(() => {
-            setVisibilitySecond(true)
-        }, 2000)
-
-        setInterval(() => {
-            setVisibilityThird(true)
-        }, 3000)
-        setInterval(() => {
-            setVisibilityFourth(true)
-        }, 4000)
-
-    return (
-        <div className="tech-page">
-            <Animated isVisible={true} animationIn='fadeInDown'>
-                <h1 style={{fontSize: '2.5rem'}}>My Technologies</h1>
-            </Animated>
-            <div class="tech-container">
-                <Animated animateOnMount={false} isVisible={visible_first} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-html5-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_first} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-css3-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_first} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-javascript-plain" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_second} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-react-original-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_second} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-sass-original" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_second} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-express-original-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_third} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-mongodb-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_third} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-postgresql-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_third} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-git-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_fourth} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-github-original-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_fourth} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-python-plain-wordmark" />
-                </Animated>
-                <Animated animateOnMount={false} isVisible={visible_fourth} animationIn='slideInLeft' className='tech-image'>
-                    <i class="devicon-swift-plain-wordmark" />
-                </Animated>
-            </div>
-        </div>
-)};
+  return (
+    <TechPageStyles>
+      <Animated isVisible={true} animationIn="fadeInDown">
+        <h1 style={{ fontSize: '2.5rem' }}>My Technologies</h1>
+      </Animated>
+      <TechPageTechContainer>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_first}
+          animationIn="slideInLeft"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-html5-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_first}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-css3-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_first}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-javascript-plain" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_second}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-react-original-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_second}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-sass-original" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_second}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-express-original-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_third}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-mongodb-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_third}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-postgresql-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_third}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-git-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_fourth}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-github-original-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_fourth}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-python-plain-wordmark" />
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_fourth}
+          animationIn="slideInLeft"
+          className="tech-image"
+          style={{ margin: '0 10%', padding: '0', color: 'blue' }}
+        >
+          <i class="devicon-swift-plain-wordmark" />
+        </Animated>
+      </TechPageTechContainer>
+    </TechPageStyles>
+  );
+};
 
 export default TechPage;
---
+++ src/pages/tech-page/tech-page.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const TechPageStyles = styled.div`
+  margin-top: 3%;
+`;
+
+export const TechPageTechContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-size: 7rem;
+  width: 100%;
+  margin-top: 2%;
+`;
