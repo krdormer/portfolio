a18c02c24f959537ecc818167b7bd73c4d42f912
krdormer <krdormer_dev@protonmail.com>
2021-07-19 10:30:55 -0500

    Add and integrate styled components package
    Adding styled-components for styling so as to avoid class collisions, a
    nd due to the semantic clarity it brings to the project. Updated HomePa
    ge component to use the new styled components format.

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
 // For styles
-import './home-page.styles.css';
-// For animations, using Animated component 
-import {Animated} from "react-animated-css";
+import { HomePageIntroTextStyles, HomePageStyles } from './home-page.styles';
+// For animations, using Animated component
+import { Animated } from 'react-animated-css';
+import { useEffect } from 'react';
 
 const HomePage = () => {
-    const [visibleFirst, setVisibilityFirst] = useState(false);
-    const [visibleSecond, setVisibilitySecond] = useState(false);
+  const [visibleFirst, setVisibilityFirst] = useState(false);
+  const [visibleSecond, setVisibilitySecond] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setVisibilityFirst(true);
+    }, 1000);
 
     setInterval(() => {
-        setVisibilityFirst(true);
-    }, 1000)
+      setVisibilitySecond(true);
+    }, 2500);
+  }, []);
 
-    setInterval(() => {
-        setVisibilitySecond(true);
-    }, 3000)
-
-    return (
-    <div className="homepage">
-        <div className="intro-text">
-            <Animated isVisible animationIn='fadeInDown' >
-                <h1 className='intro-title'>Howdy, I'm Kevin!</h1>
-            </Animated>
-            <Animated animateOnMount={false} animationIn="fadeInUp" animationInDuration={2000} isVisible={visibleFirst} >
-                <p className='intro-detail'>I am a web developer from Austin, Texas.</p>
-            </Animated>
-            <Animated animateOnMount={false} animationIn="fadeInUp" animationInDuration={2000} isVisible={visibleSecond}>
-                <p className='intro-detail'>This is my portfolio, take a look around!</p>
-            </Animated>
-        </div>
-    </div>
-)};
+  return (
+    <HomePageStyles>
+      <HomePageIntroTextStyles>
+        <Animated isVisible animationIn="fadeInDown">
+          <h1 className="intro-title">Howdy, I'm Kevin!</h1>
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          animationIn="fadeInUp"
+          animationInDuration={2000}
+          isVisible={visibleFirst}
+        >
+          <p className="intro-detail">
+            I am a web developer from Austin, Texas.
+          </p>
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          animationIn="fadeInUp"
+          animationInDuration={2000}
+          isVisible={visibleSecond}
+        >
+          <p className="intro-detail">
+            Glad you're here. Please take a look around!
+          </p>
+        </Animated>
+      </HomePageIntroTextStyles>
+    </HomePageStyles>
+  );
+};
 
 export default HomePage;
---
+++ src/pages/home-page/home-page.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HomePageStyles = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+`;
+
+export const HomePageIntroTextStyles = styled.div`
+  font-size: 1.6rem;
+  margin: 15%;
+`;
