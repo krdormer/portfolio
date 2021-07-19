cf22bb1ba1301c722bbf80a88b27ed90e46b72d0
krdormer <krdormer_dev@protonmail.com>
2021-07-19 11:28:41 -0500

    Update AboutPage styles to use styled-components

--- src/pages/about-page/about-page.component.jsx
+++ src/pages/about-page/about-page.component.jsx
@@ -1,92 +1,110 @@
-// React and React hook imports 
-import React, { useState } from 'react';
+// React and React hook imports
+import React, { useState, useEffect } from 'react';
 // For styles
-import './about-page.styles.css';
-// For animations, using Animated component 
-import {Animated} from "react-animated-css";
+import {
+  AboutPageStyles,
+  AboutPageInfoStyles,
+  AboutPageListStyles,
+  AboutPageEducationListStyles,
+  AboutPageExperienceListStyles,
+  AboutPageHobbyListStyles,
+} from './about-page.styles';
+// For animations, using Animated component
+import { Animated } from 'react-animated-css';
 
 const AboutPage = () => {
-    // Sets a state hook for each <p> tag, used for timing of display on screen
-    const [visible_first, setVisibilityFirst] = useState(false)
-    const [visible_second, setVisibilitySecond] = useState(false);
-    const [visible_third, setVisibilityThird] = useState(false);
-    const [visible_fourth, setVisibilityFourth] = useState(false);
+  // Sets a state hook for each <p> tag, used for timing of display on screen
+  const [visible_first, setVisibilityFirst] = useState(false);
+  const [visible_second, setVisibilitySecond] = useState(false);
+  const [visible_third, setVisibilityThird] = useState(false);
+  const [visible_fourth, setVisibilityFourth] = useState(false);
 
-
-
-    // Timers set to appropriate hook, displays one <p/> tag per second 
+  useEffect(() => {
+    setInterval(() => {
+      setVisibilityFirst(true);
+    }, 1000);
 
     setInterval(() => {
-        setVisibilityFirst(true)
-    }, 1000)
+      setVisibilitySecond(true);
+    }, 2000);
+    setInterval(() => {
+      setVisibilityThird(true);
+    }, 3000);
 
     setInterval(() => {
-        setVisibilitySecond(true)
-    }, 2000)
+      setVisibilityFourth(true);
+    }, 4000);
+  });
 
-    setInterval(() => {
-        setVisibilityThird(true)
-    }, 3000)
-
-    setInterval(() => {
-        setVisibilityFourth(true)
-    }, 4000)
-
-    // setInterval(() => {
-    //     setVisibilityFifth(true)
-    // }, 12000)
-    
-    return (
-    <div className="about-page">
-        <div className="about-info">
-            <Animated isVisible={true} animationIn='fadeInDown'>
-                <h1>A bit about me...</h1>
+  return (
+    <AboutPageStyles>
+      <AboutPageInfoStyles>
+        <Animated isVisible={true} animationIn="fadeInDown">
+          <h1>A bit about me...</h1>
+        </Animated>
+        <Animated
+          animateOnMount={false}
+          isVisible={visible_first}
+          animationIn="fadeInDown"
+        >
+          <p>
+            I am a self-taught software developer located in Austin, TX with a
+            background in web development and its related technologies, looking
+            to break into a professional role in the industry.
+          </p>
+        </Animated>
+        <AboutPageListStyles>
+          <div className="experience about-content">
+            <Animated
+              animateOnMount={false}
+              isVisible={visible_second}
+              animationIn="fadeInDown"
+            >
+              <h4>My Past Work</h4>
+              <AboutPageExperienceListStyles>
+                <li>Ice Cream Scooper</li>
+                <li>Store Stocker</li>
+                <li>Emergency Medical Technician</li>
+                <li>Optician</li>
+                <li>Food Delivery Courier</li>
+                <li>Customer Support Specialist</li>
+                <li>Hobbyist Programmer</li>
+              </AboutPageExperienceListStyles>
             </Animated>
-            <Animated animateOnMount={false} isVisible={visible_first} animationIn='fadeInDown'>
-                <p>
-                    I am a self-taught software developer located in Austin, TX with a background in web development 
-                    and its related technologies, looking to break into a professional role in the industry. 
-                </p>
+          </div>
+          <AboutPageHobbyListStyles>
+            <Animated
+              animateOnMount={false}
+              isVisible={visible_third}
+              animationIn="fadeInDown"
+            >
+              <h4>My Hobbies</h4>
+              <ul className="hobbies-list about-content">
+                <li>Camping and Hiking</li>
+                <li>Trail Bike Riding</li>
+                <li>Running</li>
+                <li>Video Games</li>
+              </ul>
             </Animated>
-            <div className="about-list-container">
-                <div className="experience about-content">
-                    <Animated animateOnMount={false} isVisible={visible_second} animationIn='fadeInDown'>
-                        <h4>My Past Work</h4>
-                        <ul className='experience-list'>
-                            <li>Ice Cream Scooper</li>
-                            <li>Store Stocker</li>
-                            <li>Emergency Medical Technician</li>
-                            <li>Optician</li>
-                            <li>Food Delivery Courier</li>
-                            <li>Customer Support Specialist</li>
-                            <li>Hobbyist Programmer</li>
-                        </ul>
-                    </Animated>
-                </div>
-                <div className="hobbies">
-                    <Animated animateOnMount={false} isVisible={visible_third} animationIn='fadeInDown'>
-                        <h4>My Hobbies</h4>
-                        <ul className='hobbies-list about-content'>
-                            <li>Camping and Hiking</li>
-                            <li>Trail Bike Riding</li>
-                            <li>Running</li>
-                            <li>Video Games</li>
-                        </ul>
-                    </Animated>
-                </div>
-                <div className="education">
-                    <Animated animateOnMount={false} isVisible={visible_fourth} animationIn='fadeInDown'>
-                        <h4>My Education</h4>
-                        <ul className='education-list about-content'>
-                            <li>Texas State University</li>
-                            <li>Zero To Mastery Academy</li>
-                            <li>Open Source Society University</li>
-                        </ul>
-                    </Animated>
-                </div>
-            </div>
-        </div>
-    </div>
-)};
+          </AboutPageHobbyListStyles>
+          <div className="education">
+            <Animated
+              animateOnMount={false}
+              isVisible={visible_fourth}
+              animationIn="fadeInDown"
+            >
+              <h4>My Education</h4>
+              <AboutPageEducationListStyles>
+                <li>Texas State University</li>
+                <li>Zero To Mastery Academy</li>
+                <li>Open Source Society University</li>
+              </AboutPageEducationListStyles>
+            </Animated>
+          </div>
+        </AboutPageListStyles>
+      </AboutPageInfoStyles>
+    </AboutPageStyles>
+  );
+};
 
 export default AboutPage;
---
+++ src/pages/about-page/about-page.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const AboutPageStyles = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  text-align: left;
+  margin-left: 10px;
+`;
+
+export const AboutPageInfoStyles = styled.div`
+  width: 80vw;
+  font-size: 1.4rem;
+  margin-top: 1%;
+`;
+
+export const AboutPageListStyles = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+export const AboutPageEducationListStyles = styled.div`
+  list-style-type: none;
+  padding-left: 0;
+  margin-right: 60px;
+`;
+
+export const AboutPageExperienceListStyles = styled.div`
+  list-style-type: none;
+  padding-left: 0;
+  margin-right: 60px;
+`;
+
+export const AboutPageHobbyListStyles = styled.div`
+  list-style-type: none;
+  padding-left: 0;
+  margin-right: 60px;
+`;
