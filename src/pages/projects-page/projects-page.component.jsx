6f69842264a7dca808a182f3f045288177c97208
krdormer <krdormer_dev@protonmail.com>
2021-07-19 11:50:44 -0500

    Update ProjectPage styles with styled-components

--- src/pages/projects-page/projects-page.component.jsx
+++ src/pages/projects-page/projects-page.component.jsx
@@ -1,109 +1,153 @@
 import React from 'react';
 // For styles
-import './projects-page.styles.css';
-// Material UI Button 
+import {
+  ProjectPageButtonContainerStyles,
+  ProjectPageInfoText,
+  ProjectPageProjectContainer,
+  ProjectPageProjectStyles,
+  ProjectPageProjectTitle,
+  ProjectsPageStyles,
+} from './projects-page.styles';
 import Button from '@material-ui/core/Button';
-// SVG image import as component 
+// SVG image import as component
 import { ReactComponent as CrwnImage } from '../../assets/crwn_clothing_tag.svg';
-import { ReactComponent as  DigitalMagicImage} from '../../assets/digital_magic_tag.svg';
-import { ReactComponent as RoboJokeTellerImage} from '../../assets/robo_joke_teller_image.svg';
+import { ReactComponent as DigitalMagicImage } from '../../assets/digital_magic_tag.svg';
+import { ReactComponent as RoboJokeTellerImage } from '../../assets/robo_joke_teller_image.svg';
 // For routing to project links
 import { Link } from 'react-router-dom';
-// NPM Component imports 
+// NPM Component imports
 import { Animated } from 'react-animated-css';
 
+const svgImageStyles = {
+  width: '250px',
+  height: '200px',
+  opacity: '1',
+};
+
 const ProjectsPage = () => (
-    <div className="projects-page">
-        <Animated isVisible={true} animationIn='fadeInDown'>
-            <h1>My Projects</h1>
-        </Animated>
-        <Animated isVisible={true} animationIn='fadeInDown'>
-            <div className="project-text">
-                <p>
-                    These are some of my favorite projects to date. Each will have a live demo associated with
-                    it, as well as a link to the GitHub repo where you can review the code if interested. 
-                </p>
-                <p>
-                    Feel free to take a look, play around with them, break them; I'm always open to feedback
-                    so I can produce the highest-quality software I can! 
-                </p>
-            </div>
-        </Animated>
-        <div className="project-container">
-            <Animated isVisible animationIn='slideInLeft'>
-                <div className='project'>
-                    <h6 className='project-title' >CRWN Clothing</h6>
-                    <CrwnImage className="project-image" />
-                    <div className="project-button-container">
-                        <Link to={{ 
-                            pathname: "https://crwn-clothing-21-krd.herokuapp.com/" 
-                        }} target="_blank" className='project-button-link'>
-                            <Button variant="outlined" color="primary">
-                                Live Demo!
-                            </Button>
-                        </Link>
-                    </div>
-                    <div className="project-button-container">
-                        <Link to={{ 
-                            pathname: "https://github.com/krdormer/crwn_clothing" 
-                        }} target="_blank" className='project-button-link'>
-                            <Button variant="outlined" color="primary">
-                                GitHub Link
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </Animated>
-            <Animated isVisible animationIn='slideInLeft'>
-                <div className="project">
-                    <h6 className='project-title' >Digital Magic</h6>
-                    <DigitalMagicImage className='project-image' />
-                    <div className="project-button-container">
-                        <Link to={{ 
-                            pathname: "https://relaxed-wescoff-b22e85.netlify.app/" 
-                        }} target="_blank" className='project-button-link'>
-                            <Button variant="outlined" color="primary">
-                                Live Demo!
-                            </Button>
-                        </Link>
-                    </div>
-                    <div className="project-button-container">
-                        <Link to={{ 
-                            pathname: "https://github.com/krdormer/DigitalMagic" 
-                        }} target="_blank" className='project-button-link'>
-                            <Button variant="outlined" color="primary">
-                                GitHub Link
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </Animated>
-            <Animated isVisible animationIn='slideInLeft'>
-            <div className="project">
-                <h6 className='project-title' >Robo-Joke-Teller</h6>
-                <RoboJokeTellerImage className='project-image' />
-                <div className="project-button-container">
-                    <Link to={{ 
-                        pathname: "https://jovial-benz-9278be.netlify.app/" 
-                    }} target="_blank" className='project-button-link'>
-                        <Button variant="outlined" color="primary">
-                            Live Demo!
-                        </Button>
-                    </Link>
-                </div>
-                <div className="project-button-container">
-                    <Link to={{ 
-                        pathname: "https://github.com/krdormer/joke-teller-bot" 
-                    }} target="_blank" className='project-button-link'>
-                        <Button variant="outlined" color="primary">
-                            GitHub Link
-                        </Button>
-                    </Link>
-                </div>
-            </div>
-        </Animated>
-        </div>
-        </div>
+  <ProjectsPageStyles>
+    <Animated isVisible={true} animationIn="fadeInDown">
+      <h1>My Projects</h1>
+    </Animated>
+    <Animated isVisible={true} animationIn="fadeInDown">
+      <ProjectPageInfoText>
+        <p>
+          These are some of my favorite projects to date. Each will have a live
+          demo associated with it, as well as a link to the GitHub repo where
+          you can review the code if interested.
+        </p>
+        <p>
+          Feel free to take a look, play around with them, break them; I'm
+          always open to feedback so I can produce the highest-quality software
+          I can!
+        </p>
+      </ProjectPageInfoText>
+    </Animated>
+    <ProjectPageProjectContainer>
+      <Animated isVisible animationIn="slideInLeft">
+        <ProjectPageProjectStyles>
+          <ProjectPageProjectTitle>CRWN Clothing</ProjectPageProjectTitle>
+          <CrwnImage style={svgImageStyles} />
+          <ProjectPageButtonContainerStyles>
+            <Link
+              to={{
+                pathname: 'https://crwn-clothing-21-krd.herokuapp.com/',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                Live Demo!
+              </Button>
+            </Link>
+          </ProjectPageButtonContainerStyles>
+          <div className="project-button-container">
+            <Link
+              to={{
+                pathname: 'https://github.com/krdormer/crwn_clothing',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                GitHub Link
+              </Button>
+            </Link>
+          </div>
+        </ProjectPageProjectStyles>
+      </Animated>
+      <Animated isVisible animationIn="slideInLeft">
+        <ProjectPageProjectStyles>
+          <ProjectPageProjectTitle>Digital Magic</ProjectPageProjectTitle>
+          <DigitalMagicImage style={svgImageStyles} />
+          <ProjectPageButtonContainerStyles>
+            <Link
+              to={{
+                pathname: 'https://relaxed-wescoff-b22e85.netlify.app/',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                Live Demo!
+              </Button>
+            </Link>
+          </ProjectPageButtonContainerStyles>
+          <div className="project-button-container">
+            <Link
+              to={{
+                pathname: 'https://github.com/krdormer/DigitalMagic',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                GitHub Link
+              </Button>
+            </Link>
+          </div>
+        </ProjectPageProjectStyles>
+      </Animated>
+      <Animated isVisible animationIn="slideInLeft">
+        <ProjectPageProjectStyles>
+          <ProjectPageProjectTitle>Robo-Joke-Teller</ProjectPageProjectTitle>
+          <RoboJokeTellerImage style={svgImageStyles} />
+          <div className="project-button-container">
+            <Link
+              to={{
+                pathname: 'https://jovial-benz-9278be.netlify.app/',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                Live Demo!
+              </Button>
+            </Link>
+          </div>
+          <ProjectPageButtonContainerStyles>
+            <Link
+              to={{
+                pathname: 'https://github.com/krdormer/joke-teller-bot',
+              }}
+              target="_blank"
+              className="project-button-link"
+              style={{ textDecoration: 'none' }}
+            >
+              <Button variant="outlined" color="primary">
+                GitHub Link
+              </Button>
+            </Link>
+          </ProjectPageButtonContainerStyles>
+        </ProjectPageProjectStyles>
+      </Animated>
+    </ProjectPageProjectContainer>
+  </ProjectsPageStyles>
 );
 
 export default ProjectsPage;
---
+++ src/pages/projects-page/projects-page.styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const ProjectsPageStyles = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  text-align: center;
+  font-size: 1.4rem;
+  margin-top: 5%;
+`;
+
+export const ProjectPageInfoText = styled.div`
+  width: 80vw;
+  font-size: 1.4rem;
+  margin-left: 10vw;
+  margin-right: 10vw;
+  margin-top: 2%;
+`;
+
+export const ProjectPageProjectContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  margin-top: 5%;
+`;
+
+export const ProjectPageProjectStyles = styled.div`
+  display: inline-block;
+  border: 2px solid blue;
+  border-radius: 10px;
+  width: 250px;
+  height: auto;
+  padding: 5%;
+  opacity: 0.7;
+`;
+
+export const ProjectPageButtonContainerStyles = styled.div`
+  display: inline;
+  width: 100%;
+`;
+
+export const ProjectPageProjectTitle = styled.h6`
+  font-size: 1.5rem;
+  margin: 0;
+  padding: 0;
+`;
