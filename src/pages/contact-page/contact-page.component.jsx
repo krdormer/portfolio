716d6b2f179d0474ba05bd6784509613be9ab4ee
krdormer <krdormer_dev@protonmail.com>
2021-07-19 12:02:18 -0500

    Update ContactPage to use styled-components

--- src/pages/contact-page/contact-page.component.jsx
+++ src/pages/contact-page/contact-page.component.jsx
@@ -1,4 +1,10 @@
-import './contact-page.styles.css';
+// For styling
+import {
+  ContactPageContactIconContainer,
+  ContactPageContactIconStyles,
+  ContactPageContactTextStyles,
+  ContactPageStyles,
+} from './contact-page.styles';
 
 import { ReactComponent as LinkedInLogo } from '../../assets/linkedin_icon.svg';
 import { ReactComponent as GithubLogo } from '../../assets/github_icon.svg';
@@ -8,34 +14,43 @@ import { Link } from 'react-router-dom';
 import { Animated } from 'react-animated-css';
 
 const ContactPage = () => (
-    <div className="contact-page">
-        <Animated isVisible={true} animationIn='fadeInDown'>
-            <h1 style={{fontSize: '2.5rem'}}>Let's Connect!</h1>
-        </Animated>
-        <Animated isVisible={true} animationIn='fadeInDown'>
-            <p className='contact-text'>
-                I'm always looking to meet up with other folks in the industry! If you want to collaborate
-                on a project, have resources or other cool things you want to share, or if you just want to
-                chat don't hesitate to reach out! 
-            </p>
-        </Animated>
-        <div className="contact-icon-container">
-            <div className="contact-icon">
-                <Link to={{ 
-                    pathname: "https://github.com/krdormer" 
-                }} target="_blank" className='project-button-link'>
-                    <GithubLogo />
-                </Link>
-            </div>
-            <div className="contact-icon">
-                <Link to={{ 
-                    pathname: "https://www.linkedin.com/in/kevin-dormer-637310195/" 
-                }} target="_blank" className='project-button-link'>
-                    <LinkedInLogo />
-                </Link>
-            </div>
-        </div>
-    </div>
+  <ContactPageStyles>
+    <Animated isVisible={true} animationIn="fadeInDown">
+      <h1 style={{ fontSize: '2.5rem' }}>Let's Connect!</h1>
+    </Animated>
+    <Animated isVisible={true} animationIn="fadeInDown">
+      <ContactPageContactTextStyles>
+        I'm always looking to meet up with other folks in the industry! If you
+        want to collaborate on a project, have resources or other cool things
+        you want to share, or if you just want to chat don't hesitate to reach
+        out!
+      </ContactPageContactTextStyles>
+    </Animated>
+    <ContactPageContactIconContainer>
+      <ContactPageContactIconStyles>
+        <Link
+          to={{
+            pathname: 'https://github.com/krdormer',
+          }}
+          target="_blank"
+          className="project-button-link"
+        >
+          <GithubLogo />
+        </Link>
+      </ContactPageContactIconStyles>
+      <ContactPageContactIconStyles>
+        <Link
+          to={{
+            pathname: 'https://www.linkedin.com/in/kevin-dormer-637310195/',
+          }}
+          target="_blank"
+          className="project-button-link"
+        >
+          <LinkedInLogo />
+        </Link>
+      </ContactPageContactIconStyles>
+    </ContactPageContactIconContainer>
+  </ContactPageStyles>
 );
 
 export default ContactPage;
---
+++ src/pages/contact-page/contact-page.styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ContactPageStyles = styled.div`
+  margin-top: 5%;
+`;
+
+export const ContactPageContactIconContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContactPageContactTextStyles = styled.p`
+  width: 80vw;
+  margin-top: 5%;
+  margin-left: 10vw;
+  margin-right: 10vw;
+  font-size: 1.6rem;
+`;
+
+export const ContactPageContactIconStyles = styled.div`
+  width: 10vw;
+  margin: 5% 3%;
+`;
