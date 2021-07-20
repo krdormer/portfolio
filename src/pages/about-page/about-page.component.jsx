d0f8a2579ba9388517c845a175cb6bcc64d6f023
krdormer <krdormer_dev@protonmail.com>
2021-07-20 12:49:40 -0500

    Update AboutPage styles and animations

--- src/pages/about-page/about-page.component.jsx
+++ src/pages/about-page/about-page.component.jsx
@@ -20,15 +20,22 @@ const AboutPage = () => {
   // Sets a state hook for each <p> tag, used for timing of display on screen
   const [visible_first, setVisibilityFirst] = useState(false);
   const [visible_second, setVisibilitySecond] = useState(false);
+  const [visible_third, setVisibilityThird] = useState(false);
+  const [visible_fourth, setVisibilityFourth] = useState(false);
 
   useEffect(() => {
     setInterval(() => {
       setVisibilityFirst(true);
     }, 1000);
-
     setInterval(() => {
       setVisibilitySecond(true);
     }, 2000);
+    setInterval(() => {
+      setVisibilityThird(true);
+    }, 3000);
+    setInterval(() => {
+      setVisibilityFourth(true);
+    }, 4000);
   });
 
   return (
@@ -48,12 +55,12 @@ const AboutPage = () => {
             to break into a professional role in the industry.
           </AboutPageIntroTextStyles>
         </Animated>
-        <Animated
-          animateOnMount={false}
-          isVisible={visible_second}
-          animationIn="fadeInLeft"
-        >
-          <AboutPageListStyles>
+        <AboutPageListStyles>
+          <Animated
+            animateOnMount={false}
+            isVisible={visible_second}
+            animationIn="fadeInDown"
+          >
             <AboutPageExperienceListStyles>
               <AboutPageSubTitleStyles>My Past Work</AboutPageSubTitleStyles>
               <AboutPageListItemStyles>
@@ -74,6 +81,12 @@ const AboutPage = () => {
                 Hobbyist Programmer
               </AboutPageListItemStyles>
             </AboutPageExperienceListStyles>
+          </Animated>
+          <Animated
+            animateOnMount={false}
+            isVisible={visible_third}
+            animationIn="fadeInDown"
+          >
             <AboutPageHobbyListStyles>
               <AboutPageSubTitleStyles>My Hobbies</AboutPageSubTitleStyles>
               <AboutPageListItemStyles>
@@ -85,6 +98,12 @@ const AboutPage = () => {
               <AboutPageListItemStyles>Running</AboutPageListItemStyles>
               <AboutPageListItemStyles>Video Games</AboutPageListItemStyles>
             </AboutPageHobbyListStyles>
+          </Animated>
+          <Animated
+            animateOnMount={false}
+            isVisible={visible_fourth}
+            animationIn="fadeInDown"
+          >
             <AboutPageEducationListStyles>
               <AboutPageSubTitleStyles>My Education</AboutPageSubTitleStyles>
               <AboutPageListItemStyles>
@@ -97,8 +116,8 @@ const AboutPage = () => {
                 Open Source Society University
               </AboutPageListItemStyles>
             </AboutPageEducationListStyles>
-          </AboutPageListStyles>
-        </Animated>
+          </Animated>
+        </AboutPageListStyles>
       </AboutPageInfoStyles>
     </AboutPageStyles>
   );
